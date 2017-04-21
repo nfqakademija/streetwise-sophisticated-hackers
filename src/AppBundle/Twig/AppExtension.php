b6e0abf3c177edc3356a9f9dc5d6ec80a6ea8a31
eleggua999 <eleggua999@gmail.com>
2017-04-21 16:45:46 +0300

    AppExtension service modified

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -2,32 +2,70 @@
 
 namespace AppBundle\Twig;
 
-/**
- * Class AppExtension
- * @package AppBundle\Twig
- */
-class AppExtension extends \Twig_Extension
+use JavierEguiluz\Bundle\EasyAdminBundle\Configuration\ConfigManager;
+use JavierEguiluz\Bundle\EasyAdminBundle\Twig\EasyAdminTwigExtension;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
+
+class AppExtension extends EasyAdminTwigExtension
 {
+    const ADMIN_ACTIONS = ['new', 'edit', 'delete'];
+
+    private $tokenStorage;
+
+    private $decisionManager;
+
+    public function __construct(
+        ConfigManager $configManager,
+        PropertyAccessor $propertyAccessor,
+        $debug = false,
+        TokenStorageInterface $tokenStorage,
+        AccessDecisionManagerInterface $decisionManager
+    )
+    {
+        parent::__construct($configManager, $propertyAccessor, $debug);
+        $this->tokenStorage = $tokenStorage;
+        $this->decisionManager = $decisionManager;
+    }
+
     /**
-     * @return array
+     * {@inheritdoc}
      */
     public function getFunctions()
     {
-        return array(
-            new \Twig_SimpleFunction('has_common', array($this, 'hasCommonElementsFunction')),
-        );
+        $functions = parent::getFunctions();
+        $functions[] = new \Twig_SimpleFunction('custom_get_actions_for_*_item', array($this, 'getActionsForCertainItem'));
+        return $functions;
     }
 
-    /**
-     * @param array $userRoles
-     * @param array $actionRoles
-     * @return bool
-     */
-    public function hasCommonElementsFunction(array $userRoles, array $actionRoles)
+    public function getActionsForCertainItem($view, $entityName, $entity)
     {
-        $common = array_intersect($userRoles, $actionRoles);
+        if (null === $token = $this->tokenStorage->getToken()) {
+            return array();
+        }
 
-        return !empty($common);
+        $user = $token->getUser();
+
+
+        $actions = parent::getActionsForItem($view, $entityName);
+
+        if((method_exists($entity, 'getUser') && $entity->getUser()->getId() == $user->getId()) || $this->decisionManager->decide($token, ['ROLE_ADMIN'])) {
+            return $actions;
+        }
+
+        foreach($this::ADMIN_ACTIONS as $action) {
+            unset($actions[$action]);
+        }
+
+        return $actions;
     }
 
+    public function isActionEnabled($view, $action, $entityName)
+    {
+        if (null === $token = $this->tokenStorage->getToken()) {
+            return false;
+        }
+        return (parent::isActionEnabled($view, $action, $entityName) && $this->decisionManager->decide($token, ['ROLE_ADMIN']));
+    }
 }
