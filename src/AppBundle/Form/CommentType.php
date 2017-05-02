adb89244c073be5d4b6be1427203acd39c5d2fed
Darius Taroza <darius.atviras@gmail.com>
2017-05-02 13:13:45 +0300

    Comment module (#7)
    * Comments - added formType, overriden templates
    * Comments - added CommentThread. Homework entity - generated additional functions
    * Comments - fixed Comment entity, reworked newComment action
    * Comments - added CommentTrait
    * Comments - removed Voters, added unidirectional relationship

---
+++ src/AppBundle/Form/CommentType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'content',
+                TextareaType::class
+            )
+        ;
+    }
+}
