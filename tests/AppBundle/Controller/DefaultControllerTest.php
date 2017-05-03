32af9f414318d2d0e7a41d4272fa6d68cf979c4b
eleggua999 <eleggua999@gmail.com>
2017-05-03 21:19:55 +0300

    missing tests comments added

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -4,8 +4,15 @@ namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class DefaultControllerTest
+ * @package Tests\AppBundle\Controller
+ */
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * Tests index page
+     */
     public function testIndex()
     {
         $client = static::createClient();
