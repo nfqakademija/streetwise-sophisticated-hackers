e8ed148902b66ee32a8323ca36736b2164698bc0
eleggua999 <eleggua999@gmail.com>
2017-05-14 21:38:31 +0300

    easyadmin templates refactored, menus changed

--- src/AppBundle/Controller/Admin/NewsController.php
+++ src/AppBundle/Controller/Admin/NewsController.php
@@ -89,31 +89,6 @@ class NewsController extends BaseAdminController
         $easyadmin = $this->request->attributes->get('easyadmin');
         $entity = $easyadmin['item'];
 
-        $comments = $this->getEntityComments($entity);
-
-        $comment = new Comment();
-        $commentForm = $this->createForm(CommentType::class, $comment);
-        $commentForm->handleRequest($this->request);
-        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
-            $this->denyAccessUnlessGranted('new', $comment);
-
-            $comment->setAuthor($this->getUser());
-            $comment = $commentForm->getData();
-            $thread = $this->getThreadPromise($entity);
-            $comment->setThread($thread);
-            $thread->addComment($comment);
-            $em = $this->get('doctrine.orm.default_entity_manager');
-            $em->persist($comment);
-            $em->flush();
-
-            $queryParameters = [
-                'action' => 'show',
-                'entity' => $this->entity['name'],
-                'id' => $id,
-            ];
-            return $this->redirect($this->get('router')->generate('easyadmin', $queryParameters));
-        }
-
         $fields = $this->entity['show']['fields'];
         $deleteForm = $this->createDeleteForm($this->entity['name'], $id);
 
@@ -127,8 +102,6 @@ class NewsController extends BaseAdminController
             'entity' => $entity,
             'fields' => $fields,
             'delete_form' => $deleteForm->createView(),
-            'comment_form' => $commentForm->createView(),
-            'comments' => $comments,
         ));
     }
 }
