dcaecc53220c3934b5233f0df3b745c597c50960
eleggua999 <eleggua999@gmail.com>
2017-05-15 23:40:28 +0300

    comment delete action, edit stub added

---
+++ src/AppBundle/Controller/CommentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Comment;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Comment controller.
+ *
+ * @Route("comment")
+ */
+class CommentController extends Controller
+{
+    /**
+     * Displays a form to edit an existing comment entity.
+     *
+     * @Route("/{id}/edit", name="comment_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Comment $comment)
+    {
+        $this->denyAccessUnlessGranted('edit', $comment);
+
+        $deleteForm = $this->createDeleteForm($comment);
+        $editForm = $this->createForm('AppBundle\Form\CommentType', $comment);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('comment_edit', array('id' => $comment->getId()));
+        }
+
+        return $this->render('comment/edit.html.twig', array(
+            'comment' => $comment,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a comment entity.
+     *
+     * @Route("/{id}", name="comment_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Comment $comment)
+    {
+        $form = $this->createDeleteForm($comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->denyAccessUnlessGranted('delete', $comment);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($comment);
+            $em->flush();
+
+            $referer = $request->headers->get('referer');
+            return $this->redirect($referer);
+        }
+
+        return $this->render('comment/delete.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to delete a comment entity.
+     *
+     * @param Comment $comment The comment entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Comment $comment)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('comment_delete', array('id' => $comment->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
